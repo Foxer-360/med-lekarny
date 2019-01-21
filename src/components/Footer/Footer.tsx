5cb94a438e1d6ac83e37c47ffc1b9bb56a8fbd64
studioo <studioo@example.org>
2019-01-21 14:22:27 +0100

    complete Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,44 @@
 import * as React from 'react';
 import Social from './components/Social';
 import HelpPopup from './components/HelpPopup';
+import Link from '@source/partials/Link';
 
-class Footer extends React.Component<any, any> {
+interface Link {
+  title: string;
+  url: LooseObject;
+}
+
+interface Icon {
+  title: string;
+  url: LooseObject;
+}
+
+export interface FooterProps {
+  data: {
+    links: Link[];
+    icons: Icon[];
+    copyrights: string;
+    gdprText: string;
+    gdprUrl: LooseObject;
+  };
+}
+
+export interface FooterState { }
+
+class Footer extends React.Component<FooterProps, FooterState> {
+  constructor(props: FooterProps) {
+    super(props);
+  }
+
   public render() {
+    const { 
+      links, 
+      icons, 
+      copyrights,
+      gdprText,
+      gdprUrl
+    } = this.props.data;
+
     return (
       <footer>
         <HelpPopup />
@@ -11,11 +46,15 @@ class Footer extends React.Component<any, any> {
           <div className={'container'}>
             <div>
               <ul className={'footer__list'}>
-                <li><a href="#">O nas</a></li>
-                <li><a href="#">Kariera</a></li>
-                <li><a href="#">Kontakty</a></li>
+                {links && links.map((link, i) => (
+                  <li>
+                    <Link key={i} url={link.url && link.url}>
+                      {link.title && link.title}
+                    </Link>
+                  </li>
+                ))}
               </ul>
-              <Social />
+              <Social icons={icons} />
             </div>
           </div>
           <div className={'footer__left'}/>
@@ -25,10 +64,10 @@ class Footer extends React.Component<any, any> {
         <div className="bottom">
           <div className="container">
             <div className="bottom__copyrights grid">
-              <p>© 2018 - MEDICON a.s.</p>
-              <a href="#">
-                <p>Prohlášení o ochraně osobních údajů</p>
-              </a>
+              <p>{copyrights && copyrights}</p>
+              <Link url={gdprUrl && gdprUrl} >
+                {gdprText && gdprText}
+              </Link>
             </div>
           </div>
         </div>
--- src/components/Footer/components/Social/Social.tsx
+++ src/components/Footer/components/Social/Social.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import SvgIcon from '@source/partials/SvgIcon';
+import Link from '@source/partials/Link';
 
-export interface SocialProps {}
+export interface SocialProps {
+  icons: Array<any>;
+}
 
-const Social = (props: SocialProps) => (
-  <div className="social">
-    <a href="#"><SvgIcon type={'white'} name="fb" /></a>
-  </div>
-);
+const Social = (props: SocialProps) => {
+  const { icons } = props;
+
+  return (
+    <div className={'social'}>
+      {icons && icons.map((icon, i) => (
+        <Link key={i} url={icon.url && icon.url}>
+          <SvgIcon type={'white'} name={icon.title && icon.title} />
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default Social;

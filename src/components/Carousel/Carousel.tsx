b0a968aaf157f1b60879c2ffa080f9ca127ce07e
studioo <studioo@example.org>
2019-01-28 18:16:36 +0100

    fix bugs with Carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -16,6 +16,8 @@ export interface CarouselProps {
 
 export interface CarouselState {
   currentIndex: number;
+  itemsInSlide: number;
+  galleryItems: any;
 }
 
 class Carousel extends React.Component<CarouselProps, CarouselState> {
@@ -24,33 +26,65 @@ class Carousel extends React.Component<CarouselProps, CarouselState> {
 
     this.state = {
       currentIndex: 0,
+      itemsInSlide: 1,
+      galleryItems: this.galleryItems(),
     };
   }
   
+  slideTo = (i) => {
+    console.log(`currentIndex ` + this.state.currentIndex);
+    console.log(`index ` + i);
+    this.setState({ currentIndex: i });
+  }
+
+  galleryItems() {
+    const { slides } = this.props.data;
+    let images = [];
+    
+    if (slides) {
+      slides.map((slide, i) => {
+        if (slide.image) {
+          images.push(<Media key={i} type={'image'} data={slide.image} />);
+        }
+      });
+    }
+    return images;
+  }
+
   public render() {
     const { slides } = this.props.data;
-    
+    console.log(this.state.currentIndex);
     return (
       <div className={'carousel'}>
         <div className={'carousel__images'}>
           <AliceCarousel 
-            infinite={true} 
-            autoPlay={true} 
+            autoPlay={true}
             dotsDisabled={true}
             buttonsDisabled={true}
-            autoPlayInterval={5000}
-            stopAutoPlayOnHover={true}
-            stagePadding={{paddingLeft: 0, paddingRight: 0}}
-          >
-            {slides && slides.map((slide, i) => {
-              return slide.image ? <Media key={i} type={'image'} data={slide.image} /> : '';
-            })}
-          </AliceCarousel>
+            autoPlayInterval={2500}
+            items={this.state.galleryItems}
+            onSlideChanged={(e) => {
+              this.setState({ currentIndex: e.item }); 
+            }}
+            slideToIndex={this.state.currentIndex}
+          />
         </div>
+
         <div className={'carousel__titles'}>
           <ul>
             {slides && slides.map((slide, i) => (
-              <li key={i} className={i === 0 ? 'carousel__titles__title--active' : ''}>
+              <li 
+                key={i} 
+                onClick={() => this.slideTo(i)}
+                style={i === this.state.currentIndex ? { 
+                  color: '#3eac49', 
+                  fontWeight: 700,
+                  backgroundColor: 'white',
+                  boxShadow: '0 0 40px rgba(0, 0, 0, 0.1)',
+                  borderBottom: 'none !important'
+                 } : {}} 
+                // className={i === this.state.currentIndex ? 'carousel__titles__title--active' : ''}
+              >
                 {slide.title && slide.title}
               </li>
             ))}

4aca0bd512e02ac7a186e4b30bf67099bcf87be7
Nick K <studioo@mail.ru>
2019-02-04 11:20:18 +0100

    Update Footer: add custom query for navigation items

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,10 +3,48 @@ import Social from './components/Social';
 import HelpPopup from './components/HelpPopup';
 import Link from '@source/partials/Link';
 
-interface Link {
-  title: string;
-  url: LooseObject;
-}
+import { Link as DomLink } from 'react-router-dom';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { adopt } from 'react-adopt';
+
+const GET_CONTEXT = gql`
+  {
+    languageData @client
+    pageData @client
+    websiteData @client
+    languagesData @client
+    navigationsData @client
+  }
+`;
+
+const GET_PAGES_URLS = gql`
+  query pagesUrls($language: ID!) {
+    pagesUrls(where: { language: $language }) {
+      id
+      page
+      url
+      name
+      description
+    }
+  }
+`;
+
+const ComposedQuery = adopt({
+  context: ({ render }) => <Query query={GET_CONTEXT}>{({ data }) => render(data)}</Query>,
+  getPagesUrls: ({ render, context: { languageData } }) => {
+    if (!languageData) {
+      return render({});
+    }
+    return (
+      <Query query={GET_PAGES_URLS} variables={{ language: languageData.id }}>
+        {data => {
+          return render(data);
+        }}
+      </Query>
+    );
+  },
+});
 
 interface Icon {
   title: string;
@@ -14,8 +52,11 @@ interface Icon {
 }
 
 export interface FooterProps {
+  navigations?: LooseObject;
+  languages?: LooseObject;
+  languageCode?: string;
+
   data: {
-    links: Link[];
     icons: Icon[];
     copyrights: string;
     gdprText: string;
@@ -31,8 +72,7 @@ class Footer extends React.Component<FooterProps, FooterState> {
   }
 
   public render() {
-    const { 
-      links, 
+    const {
       icons, 
       copyrights,
       gdprText,
@@ -40,39 +80,98 @@ class Footer extends React.Component<FooterProps, FooterState> {
     } = this.props.data;
 
     return (
-      <footer>
-        <HelpPopup />
-        <div className={'footer'}>
-          <div className={'container'}>
-            <div>
-              <ul className={'footer__list'}>
-                {links && links.map((link, i) => (
-                  <li>
-                    <Link key={i} url={link.url && link.url}>
-                      {link.title && link.title}
+      <ComposedQuery>
+        {({ getPagesUrls: { loading, error, data }, context }) => {
+          if (!context.navigationsData || !context.languageData || !context.languagesData || !data || !data.pagesUrls) {
+            return <div>Loading...</div>;
+          }
+
+          if (error) {
+            return `Error...${error}`;
+          }
+
+          const {
+            navigationsData: navigations,
+            languageData: { code: languageCode },
+          } = context;
+
+          const transformedNavigations = this.transformNavigationsIntoTree(navigations, data.pagesUrls);
+
+          const bottomNav = 'bottom';
+
+          const bottomNavItems =
+            transformedNavigations && transformedNavigations[bottomNav] ? transformedNavigations[bottomNav] : [];
+
+          return (
+            <footer>
+              <HelpPopup />
+              <div className={'footer'}>
+                <div className={'container'}>
+                  <div>
+                    <ul className={'footer__list'}>
+                      {bottomNavItems && bottomNavItems.map((navItem, i) => (
+                        <li key={i}>
+                          <DomLink to={navItem.url ? navItem.url : ''}>{navItem.name || navItem.title}</DomLink>
+                        </li>
+                      ))}
+                    </ul>
+                    <Social icons={icons} />
+                  </div>
+                </div>
+                <div className={'footer__left'}/>
+                <div className={'footer__right'}/>
+              </div>
+              
+              <div className="bottom">
+                <div className="container">
+                  <div className="bottom__copyrights grid">
+                    <p>{copyrights && copyrights}</p>
+                    <Link url={gdprUrl && gdprUrl} >
+                      {gdprText && gdprText}
                     </Link>
-                  </li>
-                ))}
-              </ul>
-              <Social icons={icons} />
-            </div>
-          </div>
-          <div className={'footer__left'}/>
-          <div className={'footer__right'}/>
-        </div>
-        
-        <div className="bottom">
-          <div className="container">
-            <div className="bottom__copyrights grid">
-              <p>{copyrights && copyrights}</p>
-              <Link url={gdprUrl && gdprUrl} >
-                {gdprText && gdprText}
-              </Link>
-            </div>
-          </div>
-        </div>
-      </footer>
+                  </div>
+                </div>
+              </div>
+            </footer>
+          );
+        }}
+      </ComposedQuery>
     );
+  }
+
+  private transformNavigationsIntoTree(navigation: LooseObject[], urls: LooseObject[]): LooseObject | null {
+    const tree = {};
+    if (!navigation || navigation.length < 1) {
+      return null;
+    }
+    navigation.forEach((nav: LooseObject) => {
+      tree[nav.name] = this.buildNavTree(nav.nodes, null, urls);
+    });
+    return tree;
+  }
+  private buildNavTree(nav: LooseObject[], parent: string | null, urls: LooseObject[]): LooseObject[] {
+    const res = [] as LooseObject[];
+    nav.forEach((node: LooseObject) => {
+      if (node.parent === parent) {
+        const url = urls.find((u: LooseObject) => u.page === node.page);
+        const item = {
+          ...node,
+          ...url,
+        } as LooseObject;
+        if (node.page) {
+          const children = this.buildNavTree(nav, node.page, urls);
+          if (children && children.length > 0) {
+            item.children = children;
+          }
+        }
+        if (node.title && node.link) {
+          item.url = node.link;
+        }
+        res.push(item);
+      }
+    });
+    res.sort((a: LooseObject, b: LooseObject) => a.order - b.order);
+    return res;
   }
 }
 

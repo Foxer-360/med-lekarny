782da7a1be89eb4ebc0a54378c70b482994b0625
Nick <studioo1533@gmail.com>
2019-03-13 13:06:04 +0100

    Bugfix/PromotionsAndDiscounts (#48)
    * Update PromotionAndDiscounts component: fix SSR bug, remove alice-carousel
    
    * Update Gallery: SSR bugfix, remove alice-carousel
    
    * Update PromotionAndDiscounts component: fix SSR bug, remove alice-carousel

--- src/components/PromotionsAndDiscounts/PromotionsAndDiscounts.tsx
+++ src/components/PromotionsAndDiscounts/PromotionsAndDiscounts.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import Responsive from 'react-responsive';
+
+import List from '../List';
 import Link from '@source/partials/Link';
 import Media from '@source/partials/Media';
-import List from '../List';
+import Slider from '@source/partials/Slider';
 
 interface Item {
   image: LooseObject;
@@ -17,42 +18,86 @@ export interface PromotionsAndDiscountsProps {
   };
 }
 
+const Mobile = props => <Responsive {...props} maxWidth={576} />;
+const Tablet = props => <Responsive {...props} minWidth={577} maxWidth={991} />;
+const Default = props => <Responsive {...props} minWidth={992} />;
+
 const PromotionsAndDiscounts = (props: PromotionsAndDiscountsProps) => {
   const { title, items } = props.data;
 
-  const responsive = {
-    400: { items: 1 }, 600: { items: 2 }, 1024: { items: 3 }
+  const splitArray = (array, size) => {
+    let result = [];
+    for (let i = 0; i < Math.ceil(array.length / size); i++) {
+      result[i] = (
+        <div className={'prom-and-disc__list'}>
+          {array.slice((i * size), (i * size) + size)}
+        </div>
+      );
+    }
+    return result;
   };
-  
+
   let autoPlay = false;
-  if (items.length > 3) {
+  if (items.length > 4) {
     autoPlay = true;
+  }
+
+  let arrayOfSlides = items && items.map((slide, i) => (
+    <div key={i} className={'prom-and-disc__list__item'}>
+      <Link url={slide.url && slide.url.url}>
+        {slide.image && <Media type={'image'} data={slide.image} />}
+      </Link>
+    </div>
+  ));
+
+  let arrayOfDesktopSlides = [];
+  let arrayOfTabletSlides = [];
+  let arrayOfMobiletSlides = [];
+
+  if (items.length > 3) {
+    // Split an array of 4 elements
+    arrayOfDesktopSlides = splitArray(arrayOfSlides, 3);
+  }
+  if (items.length > 2) {
+    arrayOfTabletSlides = splitArray(arrayOfSlides, 2);
+  }
+  if (items.length > 1) {
+    arrayOfMobiletSlides = splitArray(arrayOfSlides, 1);
   }
   
   return (
-    <List data={items}>
+    <List data={{}}>
       {({ data }) => (
         <div className={'prom-and-disc'}>
           <div className="container">
             {title && <h3>{title}</h3> || <div style={{ height: 50 }}/>}
-    
-            <AliceCarousel  
-              responsive={responsive}
-              dotsDisabled={true}
-              autoPlay={autoPlay}
-              autoPlayInterval={6000}
-              stopAutoPlayOnHover={true}
-            >
-              {data && data.map((item, i) => {
-                return (
-                  <div key={i} className={'prom-and-disc__item'}>
-                    <Link url={item.url && item.url.url}>
-                      {item.image && <Media type={'image'} data={item.image} />}
-                    </Link>
-                  </div>
-                );
-              })}
-            </AliceCarousel>
+            <Default>
+              <Slider 
+                autoplay={autoPlay}
+                showArrows={true}
+                delay={7000}
+                showDots={false}
+                slides={arrayOfDesktopSlides}
+              />
+            </Default>
+            <Tablet>
+              <Slider 
+                autoplay={autoPlay}
+                showArrows={true}
+                delay={7000}
+                showDots={false}
+                slides={arrayOfTabletSlides}
+              />
+            </Tablet>
+            <Mobile>
+              <Slider 
+                autoplay={autoPlay}
+                showArrows={true}
+                delay={7000}
+                showDots={false}
+                slides={arrayOfMobiletSlides}
+              />
+            </Mobile>
           </div>
         </div>
       )}

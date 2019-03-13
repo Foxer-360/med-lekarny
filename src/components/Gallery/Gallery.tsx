8e8c6ed9d480e8770e9d4a5dd728dc08f9be7f59
Nick <studioo1533@gmail.com>
2019-03-13 12:31:16 +0100

    Bugfix/Gallery (#46)
    * Update Gallery: SSR bugfix, remove alice-carousel
    
    * Update Blog: fix showmore button and some styles

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import Media from '@source/partials/Media';
+import Responsive from 'react-responsive';
+
 import List from '../List';
+import Media from '@source/partials/Media';
+import Slider from '@source/partials/Slider';
 
 interface Slide {
   image: LooseObject;
@@ -14,36 +15,84 @@ export interface GalleryProps {
   };
 }
 
+const Mobile = props => <Responsive {...props} maxWidth={576} />;
+const Tablet = props => <Responsive {...props} minWidth={577} maxWidth={991} />;
+const Default = props => <Responsive {...props} minWidth={992} />;
+
 const Gallery = (props: GalleryProps) => {
   const { slides } = props.data;
   
-  const responsive = {
-    320: { items: 1 }, 400: { items: 2 }, 700: { items: 3 }, 1024: { items: 4 }
+  const splitArray = (array, size) => {
+    let result = [];
+    for (let i = 0; i < Math.ceil(array.length / size); i++) {
+      result[i] = (
+        <div className={'gallery__list'}>
+          {array.slice((i * size), (i * size) + size)}
+        </div>
+      );
+    }
+    return result;
   };
-  
+
   let autoPlay = false;
   if (slides.length > 4) {
     autoPlay = true;
   }
 
+  let arrayOfSlides = slides && slides.map((slide, i) => (
+    <div className={'gallery__list__item'} key={i}>
+      {slide.image && <Media type={'image'} data={slide.image} />}
+    </div>
+  ));
+
+  let arrayOfDesktopSlides = [];
+  let arrayOfTabletSlides = [];
+  let arrayOfMobiletSlides = [];
+
+  if (slides.length > 4) {
+    // Split an array of 4 elements
+    arrayOfDesktopSlides = splitArray(arrayOfSlides, 4);
+  }
+  if (slides.length > 2) {
+    arrayOfTabletSlides = splitArray(arrayOfSlides, 2);
+  }
+  if (slides.length > 1) {
+    arrayOfMobiletSlides = splitArray(arrayOfSlides, 1);
+    console.log(arrayOfMobiletSlides);
+  }
+
   return (
-    <List data={slides}>
+    <List data={{}}>
       {({ data }) => (
         <div className={'gallery'}>
           <div className={'container'}>
-            <AliceCarousel  
-              responsive={responsive}
-              dotsDisabled={true}
-              autoPlay={autoPlay}
-              autoPlayInterval={7000}
-              stopAutoPlayOnHover={true}
-            >
-              {data && data.map((slide, i) => (
-                <div className={'gallery__item'} key={i}>
-                  {slide.image && <Media type={'image'} data={slide.image} />}
-                </div>
-              ))}
-            </AliceCarousel>
+            <Default>
+              <Slider 
+                autoplay={autoPlay}
+                showArrows={true}
+                delay={500000}
+                showDots={false}
+                slides={arrayOfDesktopSlides}
+              />
+            </Default>
+            <Tablet>
+              <Slider 
+                autoplay={autoPlay}
+                showArrows={true}
+                delay={500000}
+                showDots={false}
+                slides={arrayOfTabletSlides}
+              />
+            </Tablet>
+            <Mobile>
+              <Slider 
+                autoplay={autoPlay}
+                showArrows={true}
+                delay={500000}
+                showDots={false}
+                slides={arrayOfMobiletSlides}
+              />
+            </Mobile>
           </div>
         </div>
       )}

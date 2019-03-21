752381b3a37d094ee2bcca434022ec3b2bc27213
Nick K <studioo@mail.ru>
2019-03-21 12:49:15 +0100

    Fix Pharmacies: add link to google maps

--- build/components/Pharmacies/Pharmacies.js
+++ build/components/Pharmacies/Pharmacies.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import ReactMarkdown from 'react-markdown';
-import Media from '@source/partials/Media';
 import List from '../List';
+import Link from '@source/partials/Link';
+import Media from '@source/partials/Media';
 var Pharmacies = function (props) {
     var pharmacies = props.data.pharmacies;
     return (React.createElement(List, { data: pharmacies }, function (_a) {
@@ -18,6 +19,8 @@ var Pharmacies = function (props) {
                     React.createElement("br", { style: { clear: 'both' } })),
                 React.createElement("div", { className: 'pharmacies__list grid' }, data && data.map(function (pharmacie, i) { return (React.createElement("div", { key: i, className: 'pharmacies__list__item' },
                     pharmacie.image && React.createElement(Media, { type: 'image', data: pharmacie.image }),
+                    pharmacie.title && React.createElement(ReactMarkdown, { source: pharmacie.title }),
+                    React.createElement(Link, { urlNewWindow: true, url: pharmacie.addressUrl && pharmacie.addressUrl.url }, pharmacie.address && pharmacie.address),
                     pharmacie.text && React.createElement(ReactMarkdown, { source: pharmacie.text }))); })))));
     }));
 };

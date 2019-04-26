267c758dbfade34179678276171f68d990b324b2
studioo <studioo@example.org>
2019-04-26 14:06:29 +0200

    66-lékárny-detail. 64-chybná-loga-v-komponentě-pharmacieslist. Update Header top menu: fix styles

--- build/components/PharmaciesList/components/title.js
+++ build/components/PharmaciesList/components/title.js
@@ -3,7 +3,7 @@ var Title = function (props) {
     return (React.createElement("div", { className: 'pcTitle' },
         React.createElement("img", { src: "/assets/mediconLekarny/images/logo.svg", alt: "Medicon Logo" }),
         React.createElement("div", { className: 'pcTitle__title' },
-            React.createElement("h3", null, "Pharmacentrum"),
+            React.createElement("h3", null, "L\u00E9k\u00E1rna"),
             React.createElement("div", { className: 'pcTitle__title__subtitle' },
                 React.createElement("span", null, props.name),
                 React.createElement("span", { className: 'pcTitle__title__subtitle__circles' },

cd237592847a2b6a19d11f4b0ef821fe83d3c7cf
Nick <studioo1533@gmail.com>
2019-04-26 16:04:05 +0200

    Bugfix/65-oprava-zobrazování-nabídek-promotionsanddiscounts (#82)
    * Update Gallery: fix width of image

--- build/helpers/splitArray.js
+++ build/helpers/splitArray.js
@@ -2,7 +2,7 @@ import React from 'react';
 var splitArray = function (array, size, classes) {
     var result = [];
     for (var i = 0; i < Math.ceil(array.length / size); i++) {
-        result[i] = (React.createElement("div", { className: classes }, array.slice((i * size), (i * size) + size)));
+        result[i] = (React.createElement("div", { className: classes, key: i }, array.slice((i * size), (i * size) + size)));
     }
     return result;
 };
--- src/helpers/splitArray.tsx
+++ src/helpers/splitArray.tsx
@@ -5,7 +5,7 @@ const splitArray = (array, size: number, classes: string) => {
 
   for (let i = 0; i < Math.ceil(array.length / size); i++) {
     result[i] = (
-      <div className={classes}>
+      <div className={classes} key={i}>
         {array.slice((i * size), (i * size) + size)}
       </div>
     );

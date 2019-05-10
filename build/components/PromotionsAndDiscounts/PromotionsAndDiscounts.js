4d9f6bc22e1fd33541ba01a1c93d3ffbfaa4a4fc
studioo <studioo@example.org>
2019-05-10 11:17:57 +0200

    Add prefix to grid css props

--- build/components/PromotionsAndDiscounts/PromotionsAndDiscounts.js
+++ build/components/PromotionsAndDiscounts/PromotionsAndDiscounts.js
@@ -25,32 +25,17 @@ var PromotionsAndDiscounts = function (props) {
             slidesToShow: 3,
             slidesToScroll: 1,
             autoplay: true,
+            dots: false,
             pauseOnHover: true,
             arrows: true,
             responsive: [
                 {
                     breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true,
-                        dots: true
-                    }
+                    settings: { slidesToShow: 2 }
                 },
                 {
                     breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        initialSlide: 2
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
+                    settings: { slidesToShow: 1 }
                 }
             ]
         };

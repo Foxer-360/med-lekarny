c0f3aad55195a6cb5520ff3bc698098f2def8b68
Nick K <studioo@mail.ru>
2019-06-22 13:25:59 +0200

    Update Carousel with react-slick

--- build/components/Carousel/Carousel.js
+++ build/components/Carousel/Carousel.js
@@ -25,170 +25,52 @@ var __assign = (this && this.__assign) || function () {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
+var react_slick_1 = require("react-slick");
 var List_1 = require("../List");
 var Link_1 = require("../../partials/Link");
 var Media_1 = require("../../partials/Media");
-var Dots_1 = require("./components/Dots");
-var LeftArrow_1 = require("./components/LeftArrow");
-var RightArrow_1 = require("./components/RightArrow");
 var Carousel = /** @class */ (function (_super) {
     __extends(Carousel, _super);
     function Carousel(props) {
         var _this = _super.call(this, props) || this;
-        _this.componentWillReceiveProps = function (nextProps) {
-            if (_this.state.slides !== nextProps.data.slides) {
-                _this.setState({ slides: nextProps.data.slides });
-            }
-        };
-        _this.componentWillUnmount = function () {
-            if (!_this.state.pause && _this.state.autoplay) {
-                clearInterval(_this.state.interval);
-            }
-        };
-        _this.goToNextSlide = function () {
-            if (_this.state.pause) {
-                return;
-            }
-            clearInterval(_this.state.interval);
-            if (_this.state.currentIndex === _this.state.slides.length - 1) {
-                return _this.setState({
-                    currentIndex: 0,
-                    translateValue: 0,
-                    interval: setInterval(_this.goToNextSlide, _this.state.delay)
-                });
-            }
-            _this.setState({
-                currentIndex: _this.state.currentIndex + 1,
-                translateValue: _this.state.translateValue + -(_this.slideWidth()),
-                interval: setInterval(_this.goToNextSlide, _this.state.delay)
-            });
-        };
-        _this.goToPrevSlide = function () {
-            if (_this.state.pause) {
-                return;
-            }
-            clearInterval(_this.state.interval);
-            if (_this.state.currentIndex === 0) {
-                return _this.setState({
-                    currentIndex: _this.state.slides.length - 1,
-                    translateValue: (_this.state.slides.length - 1) * -(_this.slideWidth()),
-                    interval: setInterval(_this.goToNextSlide, _this.state.delay)
-                });
-            }
-            _this.setState(function (prevState) { return ({
-                currentIndex: prevState.currentIndex - 1,
-                translateValue: prevState.translateValue + _this.slideWidth(),
-                interval: setInterval(_this.goToNextSlide, _this.state.delay)
-            }); });
-        };
-        _this.goTo = function (index) {
-            if (index === _this.state.currentIndex) {
-                return;
-            }
-            clearInterval(_this.state.interval);
-            if (index > _this.state.currentIndex) {
-                _this.setState({
-                    currentIndex: index,
-                    translateValue: index * -(_this.slideWidth()),
-                    interval: setInterval(_this.goToNextSlide, _this.state.delay)
-                });
-            }
-            else {
-                _this.setState({
-                    currentIndex: index,
-                    translateValue: _this.state.translateValue + (_this.state.currentIndex - index) * (_this.slideWidth()),
-                    interval: setInterval(_this.goToNextSlide, _this.state.delay)
-                });
-            }
-        };
-        _this.slideWidth = function () {
-            if (document.querySelector('.slider__slide')) {
-                return document.querySelector('.slider__slide').clientWidth;
-            }
-            else {
-                var iFrameWidth = document.querySelector('iframe').clientWidth;
-                // fix for desktop backoffice: 75% width because '.slider__slide' 75% too (Not whole Carousel)
-                return (iFrameWidth / 100) * 75;
-            }
-        };
         _this.state = {
-            delay: 10000,
-            autoplay: true,
-            showDots: true,
-            interval: null,
-            currentIndex: 0,
-            translateValue: 0,
-            showArrows: false,
-            pause: false,
-            slides: _this.props.data.slides
+            slideIndex: 0,
+            updateCount: 0
         };
+        _this.slider = React.createRef();
         return _this;
     }
-    Carousel.prototype.componentDidMount = function () {
-        if (this.state.autoplay && !this.state.pause) {
-            var interval = setInterval(this.goToNextSlide, this.state.delay);
-            this.setState({ interval: interval });
-        }
-    };
-    Carousel.prototype.pause = function (e) {
-        e.preventDefault();
-        if (this.state.autoplay && !this.state.pause) {
-            clearInterval(this.state.interval);
-            var interval = setInterval(this.goToNextSlide, 1000000);
-            this.setState({ interval: interval, pause: true });
-        }
-    };
-    Carousel.prototype.run = function (e) {
-        e.preventDefault();
-        if (this.state.autoplay && this.state.pause) {
-            clearInterval(this.state.interval);
-            var interval = setInterval(this.goToNextSlide, this.state.delay);
-            this.setState({ interval: interval, pause: false });
-        }
-    };
-    Carousel.prototype.renderSlides = function (data) {
-        var result = [];
-        if (data) {
-            data.map(function (slide, i) {
-                if (slide.image) {
-                    result.push(React.createElement("div", { key: i, className: "slider__slide", id: 'slider__slide' },
-                        React.createElement(Link_1.default, __assign({}, slide.url),
-                            React.createElement(Media_1.default, { type: 'image', data: slide.image }))));
-                }
-            });
-        }
-        return result;
-    };
-    Carousel.prototype.renderSlider = function (data) {
-        var _this = this;
-        return (React.createElement("div", { className: "slider", onMouseEnter: function (e) { return _this.pause(e); }, onMouseLeave: function (e) { return _this.run(e); } },
-            React.createElement("div", { className: "slider__wrapper", style: {
-                    transform: "translateX(" + this.state.translateValue + "px)",
-                    transition: 'transform ease-out 0.25s'
-                } }, this.renderSlides(data)),
-            this.state.showArrows ? (React.createElement(React.Fragment, null,
-                React.createElement(LeftArrow_1.default, { goToPrevSlide: this.goToPrevSlide }),
-                React.createElement(RightArrow_1.default, { goToNextSlide: this.goToNextSlide }))) : '',
-            this.state.showDots && this.state.slides.length > 0 ?
-                React.createElement(Dots_1.default, { goTo: this.goTo, len: this.state.slides.length, currentIndex: this.state.currentIndex }) : ''));
-    };
     Carousel.prototype.render = function () {
         var _this = this;
         var displayOnTop = this.props.data.displayOnTop;
-        return (React.createElement(List_1.default, { data: this.state.slides }, function (_a) {
-            var data = _a.data;
-            return (React.createElement("div", null,
-                displayOnTop ? React.createElement("div", { className: 'carousel__divider' }) : '',
-                React.createElement("div", { className: 'carousel' },
-                    React.createElement("div", { className: 'carousel__images' }, _this.renderSlider(data)),
-                    React.createElement("div", { className: 'carousel__titles' },
-                        React.createElement("ul", { className: 'carousel__titles__list' }, data && data.map(function (slide, i) { return (React.createElement("li", { key: i, onClick: function () { return _this.goTo(i); }, className: 'carousel__titles__list__item', style: i === _this.state.currentIndex ? {
-                                color: '#3eac49',
-                                backgroundColor: 'white',
-                                boxShadow: '0 0 40px rgba(0, 0, 0, 0.1)',
-                                borderBottom: 'none !important'
-                            } : {} },
-                            React.createElement("i", null, slide.title))); }))))));
+        return (React.createElement(List_1.default, { data: this.props.data.slides || [] }, function (_a) {
+            var slides = _a.data;
+            var arrayOfSlides = (slides && slides.map(function (slide, i) { return (React.createElement("div", { key: i, className: 'carousel__slide' },
+                React.createElement("div", { className: "carousel__slide__item" },
+                    React.createElement(Link_1.default, __assign({}, slide.url),
+                        React.createElement(Media_1.default, { type: 'image', data: slide.image }))))); })) || [];
+            var settings = {
+                speed: 1000,
+                dots: true,
+                arrows: false,
+                autoplay: true,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                pauseOnHover: true,
+                beforeChange: function (current, next) { return _this.setState({ slideIndex: next }); },
+                afterChange: function () { return _this.setState(function (state) { return ({ updateCount: state.updateCount + 1 }); }); }
+            };
+            return (React.createElement("div", { className: "carousel " + (displayOnTop ? 'sliderAtTop' : '') },
+                React.createElement(react_slick_1.default, __assign({ ref: function (slider) { return (_this.slider = slider); } }, settings), arrayOfSlides),
+                React.createElement("div", { className: 'carousel__titles' },
+                    React.createElement("ul", { className: 'carousel__titles__list' }, slides && slides.map(function (slide, i) { return (React.createElement("li", { key: i, onClick: function () { return _this.slider.slickGoTo(i); }, className: 'carousel__titles__list__item', style: i === _this.state.slideIndex ? {
+                            color: '#3eac49',
+                            backgroundColor: 'white',
+                            boxShadow: '0 0 40px rgba(0, 0, 0, 0.1)',
+                            borderBottom: 'none !important'
+                        } : {} },
+                        React.createElement("i", null, slide.title))); })))));
         }));
     };
     return Carousel;
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
+import Slider from 'react-slick';
 
 import List from '../List';
 import Link from '../../partials/Link';
 import Media from '../../partials/Media';
-
-import Dots from './components/Dots';
-import Slide from './components/Slide';
-import LeftArrow from './components/LeftArrow';
-import RightArrow from './components/RightArrow';
 
 interface Slide {
   image: LooseObject;
@@ -23,212 +19,68 @@ export interface CarouselProps {
 }
 
 export interface CarouselState {
-  // tslint:disable-next-line:no-any
-  interval: any;
-  // tslint:disable-next-line:no-any
-  slides: any;
-  currentIndex: number;
-  delay: number;
-  translateValue: number;
-  autoplay: boolean;
-  showDots: boolean;
-  showArrows: boolean;
-  pause: boolean;
+  slideIndex: number;
+  updateCount: number;
 }
 
 class Carousel extends React.Component<CarouselProps, CarouselState> {
+  public slider: any;
+
   constructor(props: CarouselProps) {
     super(props);
 
     this.state = {
-      delay: 10000,
-      autoplay: true,
-      showDots: true,
-      interval: null,
-      currentIndex: 0,
-      translateValue: 0,
-      showArrows: false,
-      pause: false,
-      slides: this.props.data.slides
+      slideIndex: 0,
+      updateCount: 0
     };
-  }
-  
-  componentDidMount () {
-    if (this.state.autoplay && !this.state.pause) {
-      let interval = setInterval(this.goToNextSlide, this.state.delay);
-      this.setState({ interval });
-    }
-  }
 
-  componentWillReceiveProps = (nextProps) => {
-    if (this.state.slides !== nextProps.data.slides) {
-      this.setState({ slides: nextProps.data.slides });
-    }
-  }
-
-  componentWillUnmount = () => {
-    if (!this.state.pause && this.state.autoplay) {
-      clearInterval(this.state.interval);
-    }
-  }
-
-  pause(e: any) {
-    e.preventDefault();
-    if (this.state.autoplay && !this.state.pause) {
-      clearInterval(this.state.interval);
-      let interval = setInterval(this.goToNextSlide, 1000000);
-      this.setState({ interval, pause: true });
-    }
-  }
-
-  run(e: any) {
-    e.preventDefault();
-    if (this.state.autoplay && this.state.pause) {
-      clearInterval(this.state.interval);
-      let interval = setInterval(this.goToNextSlide, this.state.delay);
-      this.setState({ interval, pause: false });
-    }
-  }
-
-  goToNextSlide = () => {
-    if (this.state.pause) { return; }
-    clearInterval(this.state.interval);
-
-    if (this.state.currentIndex === this.state.slides.length - 1) {
-      return this.setState({
-        currentIndex: 0,
-        translateValue: 0,
-        interval: setInterval(this.goToNextSlide, this.state.delay)
-      });
-    }
-
-    this.setState({
-      currentIndex: this.state.currentIndex + 1,
-      translateValue: this.state.translateValue + -(this.slideWidth()),
-      interval: setInterval(this.goToNextSlide, this.state.delay)
-    });
-  }
-
-  goToPrevSlide = () => {
-    if (this.state.pause) { return; }
-    clearInterval(this.state.interval);
-
-    if (this.state.currentIndex === 0) { 
-      return this.setState({
-        currentIndex: this.state.slides.length - 1,
-        translateValue: (this.state.slides.length - 1) * -(this.slideWidth()),
-        interval: setInterval(this.goToNextSlide, this.state.delay)
-      });
-    }
-    
-    this.setState(prevState => ({
-      currentIndex: prevState.currentIndex - 1,
-      translateValue: prevState.translateValue + this.slideWidth(),
-      interval: setInterval(this.goToNextSlide, this.state.delay)
-    }));
-  }
-
-  goTo = (index) => {
-    if (index === this.state.currentIndex) { return; }
-    clearInterval(this.state.interval);
-
-    if (index > this.state.currentIndex) {
-      this.setState({
-        currentIndex: index,
-        translateValue: index * -(this.slideWidth()),
-        interval: setInterval(this.goToNextSlide, this.state.delay)
-      });
-    } else {
-      this.setState({
-        currentIndex: index,
-        translateValue: this.state.translateValue + (this.state.currentIndex - index) * (this.slideWidth()),
-        interval: setInterval(this.goToNextSlide, this.state.delay)
-      });
-    }
-  }
-
-  slideWidth = () => {
-    if (document.querySelector('.slider__slide')) {
-      return document.querySelector('.slider__slide').clientWidth;
-    } else {
-      var iFrameWidth = document.querySelector('iframe').clientWidth;
-      // fix for desktop backoffice: 75% width because '.slider__slide' 75% too (Not whole Carousel)
-      return (iFrameWidth / 100) * 75;
-    }
-  }
-  
-  renderSlides(data: any) {  
-    let result = [];
-
-    if (data) {
-      data.map((slide, i) => {
-        if (slide.image) {
-          result.push
-          (
-            <div key={i} className="slider__slide" id={'slider__slide'}>
-              <Link {...slide.url}>
-                <Media type={'image'} data={slide.image} />
-              </Link>
-            </div>
-          );
-        }
-      });
-    }
-    return result;
-  }
-
-  renderSlider(data: any) {
-    return (
-      <div className="slider" onMouseEnter={e => this.pause(e)} onMouseLeave={e => this.run(e)}>
-        <div 
-          className="slider__wrapper"
-          style={{ 
-            transform: `translateX(${this.state.translateValue}px)`, 
-            transition: 'transform ease-out 0.25s'}}
-        >  
-          {this.renderSlides(data)}
-        </div>
-  
-        {this.state.showArrows ? (
-          <>
-            <LeftArrow goToPrevSlide={this.goToPrevSlide}/>
-            <RightArrow goToNextSlide={this.goToNextSlide}/>
-          </>
-        ) : ''}
-        
-        {this.state.showDots && this.state.slides.length > 0 ?
-          <Dots 
-            goTo={this.goTo} 
-            len={this.state.slides.length} 
-            currentIndex={this.state.currentIndex}
-          /> : ''
-        }
-      </div>
-    );
+    this.slider = React.createRef();
   }
 
   render() {
     const { displayOnTop } = this.props.data;
  
     return (
-      <List data={this.state.slides}>
-        {({ data }) => (
-          <div>
-            {displayOnTop ? <div className={'carousel__divider'} /> : ''}
+      <List data={this.props.data.slides || []}>
+        {({ data: slides }) => {
 
-            <div className={'carousel'}>      
-              <div className={'carousel__images'}>
-                {this.renderSlider(data)}
+          const arrayOfSlides = (slides && slides.map((slide, i) => (
+            <div key={i} className={'carousel__slide'}>
+              <div className="carousel__slide__item">
+                <Link {...slide.url}>
+                  <Media type={'image'} data={slide.image} />
+                </Link>
               </div>
-              
+            </div>
+          ))) || [];
+        
+          var settings = {
+            speed: 1000,
+            dots: true,
+            arrows: false,
+            autoplay: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            pauseOnHover: true,
+            beforeChange: (current, next) => this.setState({ slideIndex: next }),
+            afterChange: () => this.setState(state => ({ updateCount: state.updateCount + 1 }))
+          };
+
+          return (
+            <div className={`carousel ${displayOnTop ? 'sliderAtTop' : ''}`}>
+              <Slider ref={slider => (this.slider = slider)} {...settings}>
+                {arrayOfSlides}
+              </Slider>
+
               <div className={'carousel__titles'}>
                 <ul className={'carousel__titles__list'}>
-                  {data && data.map((slide, i) => (
+                  {slides && slides.map((slide, i) => (
                     <li 
                       key={i} 
-                      onClick={() => this.goTo(i)}
+                      onClick={() => this.slider.slickGoTo(i)}
                       className={'carousel__titles__list__item'}
-                      style={i === this.state.currentIndex ? { 
+                      style={i === this.state.slideIndex ? { 
                         color: '#3eac49', 
                         backgroundColor: 'white',
                         boxShadow: '0 0 40px rgba(0, 0, 0, 0.1)',
@@ -241,8 +93,8 @@ class Carousel extends React.Component<CarouselProps, CarouselState> {
                 </ul>
               </div>
             </div>
-          </div>
-        )}
+          );
+        }}
       </List>
     );
   }

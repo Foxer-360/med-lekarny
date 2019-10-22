e228facbc1f4bd42048565f137c0f2ce11a8344c
Petr <pesusak@gmai.com>
2019-10-22 18:21:03 +0200

    Added link REZERVACE E-RECEPTU to PharmarciesList

--- src/components/PharmaciesList/components/title.tsx
+++ src/components/PharmaciesList/components/title.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Button from '../../../partials/Button';
 
 export interface TitleProps {
   name: string;
@@ -7,20 +8,26 @@ export interface TitleProps {
 const Title = (props: TitleProps) => {
   return (
     <div className={'pcTitle'}>
-      <img src="/assets/mediconLekarny/images/logo.svg" alt="Medicon Logo" />
-
-      <div className={'pcTitle__title'}>
-        <h3>Lékárna</h3>
-        <div className={'pcTitle__title__subtitle'}>
-          <span>{props.name}</span>
-          <span className={'pcTitle__title__subtitle__circles'}>
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-          </span>
+      <div className="row">
+        <div className="col-md-6 align-content-center">
+          <img src="/assets/mediconLekarny/images/logo.svg" alt="Medicon Logo" />
+          <div className={'pcTitle__title'}>
+            <h3>Lékárna</h3>
+            <div className={'pcTitle__title__subtitle'}>
+              <span>{props.name}</span>
+              <span className={'pcTitle__title__subtitle__circles'}>
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <Button classes='btn--orangeBorder res_recipe'>rezervace e-&#8288;receptů</Button>
         </div>
       </div>
     </div>

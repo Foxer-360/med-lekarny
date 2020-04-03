4c42248ade971e9701ceb8069d17bb31e924c7b6
Petr <pesusak@gmai.com>
2020-04-03 10:15:31 +0200

    build

--- build/components/RecipeReservation/components/pharmaPlaces.js
+++ build/components/RecipeReservation/components/pharmaPlaces.js
@@ -3,18 +3,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var pharmaPlaces = [
     {
         id: 'LK1',
-        name: 'Medicon Budějovická',
+        name: 'Poliklinika Budějovická',
         address1: 'Antala Staška 1670/80',
         address2: 'Praha 4 - Krč',
         openHours: 'Po - Pá 7:00 - 19:00',
     },
     {
+        id: 'LK3',
+        name: 'Poliklinika Vysočany',
+        address1: 'Sokolovská 810/304',
+        address2: 'Praha 9 - Vysočany',
+        openHours: 'Po - Čt 7:00 - 18:00, Pá 7:00 - 16:00',
+    },
+    {
+        id: 'LK4',
+        name: 'OD DBK',
+        address1: 'Budějovická 1667/64',
+        address2: 'Praha 4 - Krč',
+        openHours: 'Po - Pá 8:00 - 20:00 So-Ne, Svátky 9:00 - 19:00',
+    },
+    {
         id: 'LK5',
-        name: 'Medicon Holešovice',
+        name: 'Poliklinika Holešovice',
         address1: 'Plynární 1617/10',
         address2: 'Praha 7 - Holešovice',
         openHours: 'Po - Čt 8:00 - 18:00, Pá 8:00 - 17:00',
-    }
+    },
 ];
 exports.default = pharmaPlaces;
 //# sourceMappingURL=pharmaPlaces.js.map
